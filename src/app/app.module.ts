4b32b5286cb11a32376b025dcbd3846d8f0adef4
Gitpradeep7 <pradeepyellamelli0@gmail.com>
2024-10-10 21:19:29 +0530

    completed part one

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+
+
 import { AppComponent } from './app.component';
 import { PageOneComponent } from './page-one/page-one.component';
 import { OneCompComponent } from './one-comp/one-comp.component';
